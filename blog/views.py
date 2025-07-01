1d487a23236beb889b76e3c48a727b92a3af7a53
HenryWashuHe <he.h@wustl.edu>
2025-07-02 00:25:28 +0800

    Add blog layout, articles, and HTML template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Article
 # Create your views here.
 def hello_world(request):
     return HttpResponse("Hello, World!")
 
+def article_content (request):
+    article=Article.objects.all()[0]
+    title=article.title
+    brief_summary=article.brief_summary
+    content=article.content
+    article_id=article.article_id
+    published_date=article.published_date
+    return_str='title: %s, brief_summary: %s, content:%s, article_id:%s, published_date:%s' %(title,brief_summary,content,article_id,published_date)
+    return HttpResponse(return_str)
